482dd1038949c0a8f839c0f4a76c0c671dc10dcf
Ubuntu <ubuntu@ip-10-10-56-127.us-west-2.compute.internal>
2020-05-01 20:02:42 +0000

    converted output format on additional CPU and Swap file tests, and removed others

--- health-check.sh
+++ health-check.sh
@@ -407,103 +407,67 @@ FindCurrentlyMountedFileSystems()
 }
 
 #=================================== Check disk usage on all mounted file systems ===================================#
+RegisterTest "CheckDiskUsage" "List mounted disk usage"
 CheckDiskUsage()
 {
-    echo -e "\n\nChecking For Disk Usage On Mounted File System[s]"
-    echo -e "$D$D"
-    echo -e "( 0-90% = OK/HEALTHY, 90-95% = WARNING, 95-100% = CRITICAL )"
-    echo -e "$D$D"
-    echo -e "Mounted File System[s] Utilization (Percentage Used):\n"
-
     COL1=$(echo "$FS_USAGE"|awk '{print $1 " "$7}')
     COL2=$(echo "$FS_USAGE"|awk '{print $6}'|sed -e 's/%//g')
 
     for i in $(echo "$COL2"); do
     {
         if [ $i -ge 95 ]; then
-            COL3="$(echo -e $i"% $CCOLOR\n$COL3")"
+            ResultOutput FAIL "disk usage exceeds 95%: $(echo $COL1 $i)%"
         elif [[ $i -ge 90 && $i -lt 95 ]]; then
-            COL3="$(echo -e $i"% $WCOLOR\n$COL3")"
+            ResultOutput WARN "disk usage high (90 - 95%): $(echo $COL1 $i)%"
         else
-            COL3="$(echo -e $i"% $GCOLOR\n$COL3")"
+            ResultOutput OK "disk usage under 90%: $(echo $COL1 $i)%"
         fi
     }
     done
-    COL3=$(echo "$COL3"|sort -k1n)
-    paste  <(echo "$COL1") <(echo "$COL3") -d' '|column -t
 }
 
 #=================================== Check for any zombie processes ===================================#
+RegisterTest "FindZombieProcesses" "Look for non-responsive UNIX processes"
 FindZombieProcesses()
 {
-    echo -e "\n\nChecking For Zombie Processes"
-    echo -e "$D"
     ps -eo stat|grep -w Z 1>&2 > /dev/null
     if [ $? == 0 ]; then
-        echo -e "Number of zombie process on the system are :" $(ps -eo stat|grep -w Z|wc -l)
-        echo -e "\n  Details of each zombie processes found   "
-        echo -e "  $D"
+    	ResultOutput WARN "Zombie processes found on system. Check log for more info."
+        local LOGOUT=("Number of zombie process on the system are : $(ps -eo stat|grep -w Z|wc -l)"
+            "\n  Details of each zombie processes found   "
+            "  $D")
         ZPROC=$(ps -eo stat,pid|grep -w Z|awk '{print $2}')
         for i in $(echo "$ZPROC"); do
-            ps -o pid,ppid,user,stat,args -p $i
+            LOGOUT+=(`ps -o pid,ppid,user,stat,args -p $i`)
         done
+        LogOutput LOGOUT[@]
     else
-        echo -e "No zombie processes found on the system."
+        ResultOutput OK "No zombie processes found on the system."
     fi
 }
 
-#=================================== Check Inode usage ===================================#
-CheckInodeUsage()
-{
-    echo -e "\n\nChecking For INode Usage"
-    echo -e "$D$D"
-    echo -e "( 0-90% = OK/HEALTHY, 90-95% = WARNING, 95-100% = CRITICAL )"
-    echo -e "$D$D"
-    echo -e "INode Utilization (Percentage Used):\n"
-
-    COL11=$(echo "$IUSAGE"|awk '{print $1" "$7}')
-    COL22=$(echo "$IUSAGE"|awk '{print $6}'|sed -e 's/%//g')
-
-    for i in $(echo "$COL22"); do
-    {
-        if [[ $i = *[[:digit:]]* ]]; then
-        {
-            if [ $i -ge 95 ]; then
-                COL33="$(echo -e $i"% $CCOLOR\n$COL33")"
-            elif [[ $i -ge 90 && $i -lt 95 ]]; then
-                COL33="$(echo -e $i"% $WCOLOR\n$COL33")"
-            else
-                COL33="$(echo -e $i"% $GCOLOR\n$COL33")"
-            fi
-        }
-        else
-            COL33="$(echo -e $i"% (Inode Percentage details not available)\n$COL33")"
-        fi
-    }
-    done
-
-    COL33=$(echo "$COL33"|sort -k1n)
-    paste  <(echo "$COL11") <(echo "$COL33") -d' '|column -t
-}
-
 #=================================== Check for SWAP Utilization ===================================#
+RegisterTest "CheckSwapUtilization" "Check the amount of swap disk usage"
 CheckSwapUtilization()
 {
-    echo -e "\n\nChecking SWAP Details"
-    echo -e "$D"
-    echo -e "Total Swap Memory in MiB : "$(grep -w SwapTotal /proc/meminfo|awk '{print $2/1024}')", in GiB : "\
-    $(grep -w SwapTotal /proc/meminfo|awk '{print $2/1024/1024}')
-    echo -e "Swap Free Memory in MiB : "$(grep -w SwapFree /proc/meminfo|awk '{print $2/1024}')", in GiB : "\
-    $(grep -w SwapFree /proc/meminfo|awk '{print $2/1024/1024}')
+    SWAP1=`echo -e "Total Swap Memory in MiB : "$(grep -w SwapTotal /proc/meminfo|awk '{print $2/1024}')", in GiB : "\
+    $(grep -w SwapTotal /proc/meminfo|awk '{print $2/1024/1024}')`
+    SWAP2=`echo -e "Swap Free Memory in MiB : "$(grep -w SwapFree /proc/meminfo|awk '{print $2/1024}')", in GiB : "\
+    $(grep -w SwapFree /proc/meminfo|awk '{print $2/1024/1024}')`
+    local LOGOUT=("\n$(echo $SWAP1)" 
+        "\n$(echo $SWAP2)"
+        "\n \n")
+    LogOutput LOGOUT[@]
 }
 
 #=================================== Check for Processor Utilization (current data) ===================================#
+RegisterTest "CheckProcessorUtilization" "Check to see the current CPU usage" "mpstat"
 CheckProcessorUtilization()
 {
-    echo -e "\n\nChecking For Processor Utilization"
-    echo -e "$D"
-    echo -e "\nCurrent Processor Utilization Summary :\n"
-    mpstat|tail -2
+    local LOGOUT=("\nCurrent CPU Utilization Summary :\n"
+        "\n`mpstat|tail -2`"
+        "\n \n")
+    LogOutput LOGOUT[@]
 }
 
 #=================================== Check for load average (current data) ===================================#
