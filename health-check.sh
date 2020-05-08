68cac05e6e275aea442574e5602f53a047192e1d
Ubuntu <ubuntu@ip-10-10-56-127.us-west-2.compute.internal>
2020-05-08 22:58:20 +0000

    Added Tim's latest functions, fixed all other functions to match the style guide

--- health-check.sh
+++ health-check.sh
@@ -134,8 +134,13 @@ IUSAGE=$(df -PThi|egrep -iw "ext4|ext3|xfs|gfs|gfs2|btrfs"|grep -v "loop"|sort -
 #=================================== PreflightChecks ===================================#
 #=================================== Check supported OS ===================================#
 RegisterTest "CheckSupportedOS" "Ensure that this is a supported operating system"
-CheckSupportedOS()
-{
+function CheckSupportedOS() {
+    # Per SDC and SCH install guides, we officially supported the OSes listed in the supported_os variable below
+    # Our products may run on other OSes, but they have not been tested and verified by StreamSets
+    # Possible results:
+    # WARN - Detected OS is not listed among StreamSets certified platforms
+    # OK - Detected OS has been certified by StreamSets
+
     SUPPORTED_OS=("CentOS release 6" "CentOS Linux release 7" 
         "Oracle Linux Server release 6" "Oracle Linux Server release 7" 
         "Red Hat Enterprise Linux Server release 6" "Red Hat Enterprise Linux Server release 7" 
@@ -146,7 +151,8 @@ CheckSupportedOS()
         OS=`[ -x /usr/bin/lsb_release ] &&  echo -e "Operating System :" $(lsb_release -d|awk -F: '{print $2}'|sed -e 's/^[ \t]*//')  || \
         echo -e "\nOperating System :" $(cat /etc/system-release)`
         FOUND="false"
-        for i in $( echo "$SUPPORTED_OS"); do
+        for i in $( echo "$SUPPORTED_OS")
+        do
         {
             if [[ "$OS" == *"$i"* ]]; then
                 FOUND="true"
@@ -167,17 +173,26 @@ CheckSupportedOS()
 
 #=================================== Check compatible JVM version ===================================#
 RegisterTest "CheckJVMVersion" "Ensure that a compatible JVM is installed and available"
-CheckJVMVersion()
-{
-    if type -p java &>/dev/null; then
+function CheckJVMVersion() {
+    # Per SDC and SCH install guides, the only currently supported JVM that hasn't reached EOL is
+    # OpenJDK 8. TODO: revisit this once JVM 11 support is officially added
+    # Possible results:
+    # FAIL - No JVM installed or detected
+    # WARN - JVM is not officially supported or has reached EOL
+    # OK - OpenJDK 8 detected and available
+    
+    if type -p java &>/dev/null
+    then
         _java=java
-    elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
+    elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]]
+    then
         _java="$JAVA_HOME/bin/java"
     else
          ResultOutput FAIL "No JVM installed. Please install OpenJDK 8."
     fi
     
-    if [[ "$_java" ]]; then
+    if [[ "$_java" ]]
+    then
         vendor=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $1}')
         version=$("$_java" -version 2>&1 | head -1 | cut -d'"' -f2 | sed '/^1\./s///' | cut -d'.' -f1)
         if [[ "$vendor" != *"openjdk"* ]]; then
@@ -187,7 +202,8 @@ CheckJVMVersion()
                 "on JVM version 8. StreamSets recommends installing OpenJDK 8."
                 "\n \n")
             LogOutput LOGOUT[@]
-        elif [[ "$version" != 8 ]]; then
+        elif [[ "$version" != 8 ]]
+        then
         	ResultOutput WARN "JVM version is not 8"
         	local LOGOUT=("We noticed that your JVM version is not 8. Unfortunately, StreamSets is not yet"
         	    "compatible with newer versions of the JVM, and older versions have reached EOL. StreamSets"
@@ -201,24 +217,125 @@ CheckJVMVersion()
 }
 
 #=================================== Check ulimit max file handle setting ===================================#
-RegisterTest "CheckUlimit" "Ensure that hard and soft open file limit setting is set to required minimum: 32768"
-CheckUlimit()
-{
-    if [[ `ulimit -n` < 32768 ]]; then
+RegisterTest "CheckUlimit" "Ensure that hard and soft open file limit setting is set to required maximum: 32768"
+function CheckUlimit() {
+    # Per SDC install guide, the maximum hard and soft file limit should be at least 32768. On most Linux OSes
+    # the default limit is 1024
+    # Possible results:
+    # FAIL - ulimit setting is too low
+    # OK - ulimit setting is good
+    
+    if [[ `ulimit -n` < 32768 ]]
+    then
         ResultOutput FAIL "Open file limit setting too low (< 32768)"
-        local LOGOUT=("StreamSets Data Collector requires minimum hard and soft open file limit setting of 32768."
+        local log_output=( "StreamSets Data Collector requires maximum hard and soft open file limit setting of 32768."
         	"See https://access.redhat.com/solutions/61334 for info on how to set file limit"
-            "\n \n")
-        LogOutput LOGOUT[@]
+            "\n \n" )
+        LogOutput log_output[@]
     else
         ResultOutput OK "Hard and soft open file limit setting is correct"
     fi
 }
 
+#=================================== Check NTP service ===================================#
+RegisterTest "SchPrereqNtp" "Verify ntp service is running and working."
+function SchPrereqNtp() {
+    # Per SCH install guide, Time must be syncrhonized using NTP
+    # Method: https://www.cyberciti.biz/faq/linux-unix-bsd-is-ntp-client-working/
+    # Possible results:
+    # ERROR - NTP is not running.  ntpq outputs "ntpq: read: Connection refused"
+    # WARN - NTP is running and reachable.  But the server is not in sync.
+    # OK - NTP is running and ntpq output indicates it is synced with a server
+
+    local testResult="FAIL"
+    local report="ntp service required but no ntp utilities found.  ntpstat missing."
+    if ( which ntpstat >/dev/null )
+    then
+        # ntpstat exists.  Use it to determine if ntp is working
+        ntpstat 2>/dev/null >/dev/null
+        stat=$?
+        case "$stat" in
+            0)
+                local ntpServer="$(ntpstat 2>/dev/null | head -n 1)"
+                if [ "$ntpServer" = "unsynchronised" ]
+                then
+                    testResult=FAIL
+                    report="ntp service is running but not in sync."
+                else
+                    testResult=OK
+                    report="ntp service is running and in sync.  $ntpServer"
+                fi
+            ;;
+            1)
+                testResult=FAIL
+                report="ntp service is running but not in sync."
+            ;;
+            2)
+                testResult=FAIL
+                report="ntp service cannot be reached and may not be running."
+            ;;
+        esac
+    elif ( which ntpq >/dev/null )
+    then
+        # ntpq exists
+        local linecount=$( ntpq -pn 2>&1 | wc -l )
+        local selected_server=$( ntpq -pn 2>/dev/null | sed '/^\*/!d; s/ .*//' )  # Selected server starts with "*"
+        if [ $linecount -le 1 ]
+        then
+            testResult=FAIL
+            report="ntp service cannot be reached and may not be running."
+        elif [ "$selected_server" = "" ]
+        then
+            testResult=FAIL
+            report="ntp service is running but not in sync with a server.  Test with: ntpq -pn"
+        else
+            testResult=OK
+            report="ntp service is running and in sync with ${selected_server}"
+        fi
+    fi
+    ResultOutput $testResult "${report}"
+}
+
+#=================================== Check CPU Thread Count ===================================#
+RegisterTest "SchPrereqCPUThreads" "Verify the number of CPU threads meets requirements"
+function SchPrereqCPUThreads() {
+    # Per SCH install guide, need at least 8 cores (threads) though 4 is okay for multiple node HA installs.
+    # Method: https://www.cyberciti.biz/faq/check-how-many-cpus-are-there-in-linux-system/
+    # Possible results:
+    # ERROR - Did not meet minimum
+    # WARN - Met minimum exactly
+    # OK - Have more than minimum
+
+    sch_min_threads=8
+    local testResult="FAIL"
+    local report="Unable to access core and thread counts in /proc/cpuinfo"
+    local num_cores=$( sed -n '/cpu cores/ {s/.* //g;p;q}' /proc/cpuinfo )
+    local num_threads=$( grep -c processor /proc/cpuinfo )
+    if [ $? -eq 0 ]
+    then
+        if [ $num_threads -lt $sch_min_threads ]
+        then
+            testResult="FAIL"
+            report="Number of CPU threads/cores is $num_threads which is less than minimum of $sch_min_threads"
+        elif [ $num_threads -eq $sch_min_threads ]
+        then
+            testResult="WARN"
+            report="Number of CPU threads/cores is $num_threads which only meets the minimum of $sch_min_threads"
+        else
+            testResult="OK"
+            report="Number of CPU threads/cores is $num_threads which exceeds the minimum of $sch_min_threads"
+        fi
+    fi
+    ResultOutput $testResult "${report}"
+}
+
 #=================================== Print Operating System Details ===================================#
 RegisterTest "PrintOSDetails" "Logs system details (Hostname, Kernel, and OS architecture)"
-PrintOSDetails()
-{
+function PrintOSDetails() {
+    # Log-only function that prints additional information about the runtime environment
+    # Possible results:
+    # N/A
+    
     HOSTNAME=`hostname -f &> /dev/null && printf "Hostname : $(hostname -f)" || printf "Hostname : $(hostname -s)"`
 
     KERNEL=$(echo -e "Kernel Version : " $(uname -r))
@@ -233,8 +350,11 @@ PrintOSDetails()
 
 #=================================== Print system uptime ===================================#
 RegisterTest "PrintSystemUptime" "Shows how long the host system has been running"
-PrintSystemUptime()
-{
+function PrintSystemUptime() {
+    # Log-only function that prints the current system runtime
+    # Possible results:
+    # N/A
+    
     UPTIME=$(uptime)
     CURR_UPTIME=`echo $UPTIME|grep day &> /dev/null`
     if [ $? != 0 ]
@@ -253,8 +373,11 @@ PrintSystemUptime()
         
 #=================================== Check for any read-only file systems ===================================#
 RegisterTest "FindReadOnlyFileSystems" "Checks to see if any read-only file systems are mounted in this environment"
-FindReadOnlyFileSystems()
-{
+function FindReadOnlyFileSystems() {
+    # Log-only function that prints any read-only file systems
+    # Possible results:
+    # N/A
+    
     READONLY=`echo "$MOUNT"|grep -w \(ro\) && echo -e "\n.....Read Only file system[s] found"|| echo -e ".....No read-only file system[s] found. "`
     local LOGOUT=("\n${READONLY}"
         "\n \n")
@@ -263,8 +386,11 @@ FindReadOnlyFileSystems()
 
 #=================================== Check for currently mounted file systems ===================================#
 RegisterTest "FindCurrentlyMountedFileSystems" "List currently mounted file systems"
-FindCurrentlyMountedFileSystems()
-{
+function FindCurrentlyMountedFileSystems() {
+    # Log-only function that prints the currently mounted file systems
+    # Possible results:
+    # N/A
+    
     CURRENT_MOUNT_FS=`echo "$MOUNT"|column -t`
     local LOGOUT=("\n${CURRENT_MOUNT_FS}"
         "\n \n")
@@ -273,8 +399,13 @@ FindCurrentlyMountedFileSystems()
 
 #=================================== Check disk usage on all mounted file systems ===================================#
 RegisterTest "CheckDiskUsage" "List mounted disk usage"
-CheckDiskUsage()
-{
+function CheckDiskUsage() {
+    # Prints disk usage and generates a warning if the OS is about to run out of disk space
+    # Possible results:
+    # FAIL: At least one mounted disk is over 95% usage
+    # WARN: At least one mounted disk is between 90 and 95% usage
+    # OK: Mounted disc usage less than 90%
+    
     COL1=$(echo "$FS_USAGE"|awk '{print $1 " "$7}')
     COL2=$(echo "$FS_USAGE"|awk '{print $6}'|sed -e 's/%//g')
 
@@ -293,8 +424,12 @@ CheckDiskUsage()
 
 #=================================== Check for any zombie processes ===================================#
 RegisterTest "FindZombieProcesses" "Look for non-responsive UNIX processes"
-FindZombieProcesses()
-{
+function FindZombieProcesses() {
+    # Prints any non-responsive UNIX processes
+    # Possible results:
+    # WARN: Non-responsive processes listed in the logs
+    # OK: No non-responsive processes found
+    
     ps -eo stat|grep -w Z 1>&2 > /dev/null
     if [ $? == 0 ]; then
     	ResultOutput WARN "Zombie processes found on system. Check log for more info."
@@ -313,8 +448,11 @@ FindZombieProcesses()
 
 #=================================== Check for SWAP Utilization ===================================#
 RegisterTest "CheckSwapUtilization" "Check the amount of swap disk usage"
-CheckSwapUtilization()
-{
+function CheckSwapUtilization() {
+    # Log-only function that prints the amount of swap space used in the system
+    # Possible results:
+    # N/A
+    
     SWAP1=`echo -e "Total Swap Memory in MiB : "$(grep -w SwapTotal /proc/meminfo|awk '{print $2/1024}')", in GiB : "\
     $(grep -w SwapTotal /proc/meminfo|awk '{print $2/1024/1024}')`
     SWAP2=`echo -e "Swap Free Memory in MiB : "$(grep -w SwapFree /proc/meminfo|awk '{print $2/1024}')", in GiB : "\
@@ -327,8 +465,11 @@ CheckSwapUtilization()
 
 #=================================== Check for Processor Utilization (current data) ===================================#
 RegisterTest "CheckProcessorUtilization" "Check to see the current CPU usage" "mpstat"
-CheckProcessorUtilization()
-{
+function CheckProcessorUtilization() {
+    # Log-only function that prints the current CPU usage
+    # Possible results:
+    # N/A
+    
     local LOGOUT=("\nCurrent CPU Utilization Summary :\n"
         "\n`mpstat|tail -2`"
         "\n \n")
@@ -337,8 +478,8 @@ CheckProcessorUtilization()
 
 #=================================== ServiceChecks ===================================#
 #=================================== Pre-calc memory settings ===================================#
-CalcMemorySettings()
-{
+function CalcMemorySettings() {
+    # Helper function that pre-calculates memory usage for specified process
     if [ -z $PID ]; then
         STREAMSETS_SDC_XMX="$(ps -f -u $OWNER | grep -Eo 'Xmx[[:digit:]]+(k|K|m|M|g|G|[[:space:]])' | sed 's/Xmx//g')"
     else
@@ -374,8 +515,12 @@ CalcMemorySettings()
 
 #=================================== Check initial and heap sizes match ===================================#
 RegisterTest "CheckMemorySettingsMatch" "Checks initial and heap sizes match."
-CheckMemorySettingsMatch()
-{
+function CheckMemorySettingsMatch() {
+    # Checks to see if the initial and maximum heap sizes match, per industry-standard recommendations
+    # Possible results:
+    # WARN: initial and maximum heap sizes do not match
+    # OK: initial and maximum heap sizes match
+    
 	CalcMemorySettings
 	
     if [ $STREAMSETS_SDC_RAW_XMS -ne $STREAMSETS_SDC_RAW_XMX ]; then
@@ -393,8 +538,14 @@ CheckMemorySettingsMatch()
 
 #=================================== Check current against min recommended memory ===================================#
 RegisterTest "CheckMinMemory" "Check current against min recommended memory"
-CheckMinMemory()
-{
+function CheckMinMemory() {
+    # Checks to see if the heap memory is at least 8 GB or 16 GB for production systems
+    # Production warning can be disabled with the -n flag
+    # Possible results:
+    # FAIL: Heap memory is below 8 GB
+    # WARN: Heap memory is below 16 GB for production systems (can be disabled)
+    # OK: At least 8 GB (non-prod) or 16 GB (prod) heap space has been allocated
+    
     CalcMemorySettings
 
     if [ $STREAMSETS_SDC_RAW_XMX -lt 8589934592 ]; then
@@ -419,8 +570,13 @@ CheckMinMemory()
 
 #=================================== Check current against max recommended memory ===================================#
 RegisterTest "CheckMaxMemory" "Check current against max recommended memory"
-CheckMaxMemory()
-{
+function CheckMaxMemory() {
+    # Check to see if more than 64 GB has been allocated. According to some industry literature, garbage collection
+    # will begin to become a bottleneck when the heap size is larger than 64 GB, regardless of which GC algorithm is used
+    # Possible results:
+    # ERROR: More than 64 GB heap space detected
+    # OK: Less than 64 GB heap space detected
+    
     CalcMemorySettings
 
     if [ $STREAMSETS_SDC_RAW_XMX -ge 68719476736 ]; then
@@ -439,8 +595,15 @@ CheckMaxMemory()
 
 #=================================== Check % of memory used by StreamSets ===================================#
 RegisterTest "CheckPctOfSysMemory" "Check that % of memory in use by StreamSets is okay." "bc"
-CheckPctOfSysMemory()
-{
+function CheckPctOfSysMemory() {
+    # Check to see if the percentage of heap memory allocated to StreamSets is more than 75% of the total available system memory.
+    # We should note that 75% is a heuristic that assumes that the StreamSets application is the only significant application
+    # deployed on the system. This percentage may need to be adjusted up or down depending on circumstances
+    # (e.g. other applications, low system memory, etc.)
+    # Possible results:
+    # WARN: More than 75% of available system memory allocated to StreamSets
+    # OK: StreamSets consumes less than 75% of available system memory
+    
     CalcMemorySettings
 
     SYSMEM="$(awk '/MemTotal/ { printf "%.0f \n", $2*1024 }' /proc/meminfo)"
